4f82b8c06f7d4a2b9a4aa98d95b748de546b5bdb
GorudenTaiga <goruden.taiga27@gmail.com>
2025-09-22 22:28:34 +0700

    Category

--- database/migrations/2025_09_08_000002_change_category_id_to_json_in_buku_table.php
+++ database/migrations/2025_09_08_000002_change_category_id_to_json_in_buku_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('buku', function (Blueprint $table) {
-            $table->dropForeign(['category_id']);
+            // $table->dropForeign(['category_id']);
             $table->dropColumn('category_id');
         });
 
