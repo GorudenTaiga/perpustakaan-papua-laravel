00fabae6a625fc50bfe843e7d3a0a2ad184cbe49
GorudenTaiga <goruden.taiga27@gmail.com>
2025-09-23 14:39:17 +0700

    .

--- app/Filament/Resources/Admin/Members/Pages/CreateMember.php
+++ app/Filament/Resources/Admin/Members/Pages/CreateMember.php
@@ -3,9 +3,23 @@
 namespace App\Filament\Resources\Admin\Members\Pages;
 
 use App\Filament\Resources\Admin\Members\MemberResource;
+use App\Models\User;
+use Carbon\Carbon;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateMember extends CreateRecord
 {
     protected static string $resource = MemberResource::class;
+    
+    protected function afterCreate() {
+        $member = $this->record;
+        $user = User::create([
+            'name' => $member->name,
+            'password' => $member->password,
+            'email' => $member->email
+        ]);
+        if ($user) {
+            $member->users_id = $user->id;
+        }
+    }
 }
