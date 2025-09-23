3d387ea0d7c4158733fee3cfc1e3e5bf8b9ff962
GorudenTaiga <goruden.taiga27@gmail.com>
2025-09-23 15:11:14 +0700

    .

--- resources/views/pages/member/kartuAnggota.blade.php
+++ resources/views/pages/member/kartuAnggota.blade.php
@@ -21,13 +21,13 @@
 
         /* Ukuran kartu: 8.6cm x 5.4cm */
         .kartu {
+            width: 8.6cm;
             height: 5.4cm;
             border: 1px solid #000;
             border-radius: 8px;
             overflow: hidden;
             position: relative;
             box-sizing: border-box;
-            /* Pastikan padding dan border dihitung dalam ukuran total */
         }
 
         /* HEADER */
@@ -59,20 +59,36 @@
 
         .card-body {
             display: flex;
-            /* Aktifkan Flexbox untuk menata foto dan detail */
+            gap: 15px;
+            align-items: flex-start;
+        }
+
+        .left-section {
+            display: flex;
+            flex-direction: column;
             align-items: center;
-            /* Posisikan elemen di tengah secara vertikal */
-            gap: 10px;
-            /* Jarak antara foto dan detail */
         }
 
         .photo img {
-            width: 70px;
-            /* Ukuran foto yang lebih jelas */
-            height: 90px;
+            width: 80px;
+            height: 100px;
             border: 1px solid #ccc;
-            object-fit: none;
-            /* Pastikan foto tidak terdistorsi */
+            object-fit: cover;
+            margin-bottom: 5px;
+        }
+
+        .barcode {
+            text-align: center;
+        }
+
+        .barcode div {
+            margin-top: 2px;
+            font-size: 9px;
+        }
+
+        .details {
+            flex-grow: 1;
+            padding-top: 5px;
         }
 
         .details table {
@@ -88,24 +104,12 @@
         /* FOOTER pakai flex agar tidak error */
         .footer {
             position: absolute;
-            /* Posisikan secara absolut di dalam .kartu */
             bottom: 5px;
             left: 10px;
             right: 10px;
             display: flex;
-            /* Aktifkan Flexbox untuk menata barcode */
             justify-content: flex-start;
-            /* Posisikan elemen di awal (kiri) */
             align-items: flex-end;
-            font-size: 9px;
-        }
-
-        .barcode {
-            text-align: center;
-        }
-
-        .barcode div {
-            margin-top: 2px;
             font-size: 9px;
         }
 
@@ -113,13 +117,6 @@
             text-align: right;
             width: 40%;
             font-size: 9px;
-        }
-
-        .ttd img {
-            width: 50px;
-            display: block;
-            margin-left: auto;
-            margin-bottom: 2px;
         }
     </style>
 </head>
@@ -139,12 +136,18 @@
 
         {{-- CONTENT --}}
         <div class="content">
-            {{-- Ini adalah wadah utama untuk foto dan detail --}}
             <div class="card-body">
-                <div class="photo">
-                    <img src="{{ 'data:image/png;base64,' . base64_encode(file_get_contents(Storage::disk('public')->url($member->image))) }}"
-                        alt="Foto Member">
+                <div class="left-section">
+                    <div class="photo">
+                        <img src="{{ 'data:image/png;base64,' . base64_encode(file_get_contents(Storage::disk('public')->url($member->image))) }}"
+                            alt="Foto Member">
+                    </div>
+                    <div class="barcode">
+                        {!! $barcode->getBarcodeHTML($member->membership_number, 'C39', 1, 20) !!}
+                        <div>{{ $member->membership_number }}</div>
+                    </div>
                 </div>
+
                 <div class="details">
                     <table>
                         <tr>
@@ -170,15 +173,7 @@
 
         {{-- FOOTER --}}
         <div class="footer">
-            <div class="barcode">
-                {!! $barcode->getBarcodeHTML($member->membership_number, 'C39', 1, 20) !!}
-                {{ $member->membership_number }}
-            </div>
-            {{-- <div class="ttd">
-                <div>Jayapura, {{ $today->isoFormat('D MMMM Y') }}</div>
-                <img src="{{ asset('signature.png') }}" alt="Tanda Tangan">
-                <div>Kepala Perpustakaan</div>
-            </div> --}}
+            {{-- Bagian ini dikosongkan karena barcode sudah dipindah --}}
         </div>
     </div>
 </body>
