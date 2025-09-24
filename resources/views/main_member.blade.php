d3a8de368404788d6006b71bbb0a54d807dda485
GorudenTaiga <goruden.taiga27@gmail.com>
2025-09-24 19:12:41 +0700

    Tambah Wishlist

--- resources/views/main_member.blade.php
+++ resources/views/main_member.blade.php
@@ -142,11 +142,11 @@
                         <ul class="list-group">
                             @foreach ($wishlist as $item)
                                 <li class="list-group-item d-flex align-items-center">
-                                    <img src="{{ Storage::disk('public')->url($item->banner) }}"
-                                        alt="{{ $item->judul }}" class="img-thumbnail me-3"
+                                    <img src="{{ Storage::disk('public')->url($item->buku->banner) }}"
+                                        alt="{{ $item->buku->judul }}" class="img-thumbnail me-3"
                                         style="width: 60px; height: 80px; object-fit: cover;">
-                                    <span>{{ $item->judul }}</span>
-                                    <a href="{{ route('buku', $item->slug) }}">
+                                    <span>{{ $item->buku->judul }}</span>
+                                    <a href="{{ route('buku', $item->buku->slug) }}">
                                         Detail
                                     </a>
                                 </li>
