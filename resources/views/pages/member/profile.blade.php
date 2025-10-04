440cbcf86cdbfced2e66aebec30e9239175f1532
GorudenTaiga <goruden.taiga27@gmail.com>
2025-10-04 07:41:53 +0700

    Tambah button zoom dan edit pada profile

--- resources/views/pages/member/profile.blade.php
+++ resources/views/pages/member/profile.blade.php
@@ -98,7 +98,8 @@
                                         <div class="modal-footer">
                                             <button type="button" class="btn btn-secondary"
                                                 data-bs-dismiss="modal">Batal</button>
-                                            <button type="submit" class="btn btn-primary">Simpan</button>
+                                            <button type="submit" class="btn btn-primary"
+                                                data-bs-dismiss="modal">Simpan</button>
                                         </div>
                                     </form>
                                 </div>
