23206d54f4711110b0f15c14a96a4af435bdbb4c
GorudenTaiga <goruden.taiga27@gmail.com>
2026-03-06 07:57:54 +0700

    refactor: update filter button text for clarity in allBuku view

--- resources/views/pages/member/allBuku.blade.php
+++ resources/views/pages/member/allBuku.blade.php
@@ -47,7 +47,8 @@
                             d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2a1 1 0 01-.293.707L13 13.414V19a1 1 0 01-.553.894l-4 2A1 1 0 017 21v-7.586L3.293 6.707A1 1 0 013 6V4z">
                         </path>
                     </svg>
-                    <span x-text="showFilters ? 'Sembunyikan Filter' : 'Tampilkan Filter'">Filter & Cari</span>
+                    <span x-text="showFilters ? 'Sembunyikan Filter & Cari' : 'Tampilkan Filter & Cari'">Tampilkan Filter &
+                        Cari</span>
                 </button>
             </div>
 
