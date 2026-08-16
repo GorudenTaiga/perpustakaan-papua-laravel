283f11606795d18d2b4193d53c3fa147afc5dc95
GorudenTaiga <goruden.taiga27@gmail.com>
2026-08-16 22:51:02 +0700

    fix: update sorting logic for average rating in index method to handle null values correctly

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -40,10 +40,10 @@ class BukuController extends Controller
                         $q->orderBy('created_at', 'asc');
                         break;
                     case 'ratingDesc':
-                        $q->orderByRaw("reviews_avg_rating IS NULL, reviews_avg_rating");
+                        $q->orderByRaw("reviews_avg_rating DESC NULLS LAST");
                         break;
                     case 'ratingAsc':
-                        $q->orderByRaw("reviews_avg_rating IS NOT NULL, reviews_avg_rating");
+                        $q->orderByRaw("reviews_avg_rating ASC NULLS FIRST");
                         break;
                 }
             })
